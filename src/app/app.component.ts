baadf9c29a377e19d9827d257142a35aff0ea5ed
Damian Gemza <d.gemza@mdbootstrap.com>
2018-09-10 11:03:08 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { ModalDirective } from 'angular-bootstrap-md';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'mdb-pwa';
+export class AppComponent implements OnInit {
+  @ViewChild('basicModal') basicModal: ModalDirective;
+
+  constructor(private swUpdate: SwUpdate) { }
+
+  ngOnInit() {
+    this.checkForUpdates();
+  }
+
+  forceUpdate() {
+    this.swUpdate.activateUpdate().then(() => {
+      document.location.reload();
+    });
+  }
+
+  checkForUpdates() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        console.log('New Version');
+        this.basicModal.show();
+      });
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ NO_ERRORS_SCHEMA ]
 })
 export class AppModule { }
